f91a101cf3db755fa25b13cc183825d503b459e8
MarkGN <mgnorrish@gmail.com>
2026-07-31 16:21:15 +1000

    add odd rooms, some trainer templates and instances

---
+++ games/demos/pokemon/authoring/dialogues_csv_to_ink.py
@@ -0,0 +1,26 @@
+def write_ink(line):
+    """# It should produce outputs like the following
+    { get("$self.money") == 0:
+    -> aftermath
+    else:
+    -> challenge}
+
+    == challenge
+    What fool dares challenge the might of the unevolved bug pokemon?!
+    ~ scenario("battle")
+    -> END
+
+    == win
+    ~ victory()
+    Huh. These are actually kind of underwhelming.
+    -> END
+
+    == post_victory
+    I have such a dumb shtick.
+    -> END"""
+
+
+# for line in trainer_dialogues:  # except the first, of course
+#     write(
+#         "../dialogue/" + line[0] + ".ink",
+#     )
